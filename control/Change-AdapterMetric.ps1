c45a035696748178eb6bc61e6d0229d993366532
yoshida-satoshi <yoshida.satoshi@outlook.com>
2023-08-22 16:07:44 +0900

    update

--- control/Change-AdapterMetric.ps1
+++ control/Change-AdapterMetric.ps1
@@ -1,5 +1,4 @@
 
 # 管理接続作業用NWのメトリックを悪化させることで、演習用NWを優先する
 # デフォルトはすべてのAdapterがMetric 10
-Get-NetAdapter -Name "[変更禁止]管理接続作業用NW" | Set-NetIPInterface -InterfaceMetric 300
-
+ Get-NetIPInterface | ?{$_.InterfaceAlias -eq "[変更禁止]管理接続作業用NW" } | Set-NetIPInterface -InterfaceMetric 300
